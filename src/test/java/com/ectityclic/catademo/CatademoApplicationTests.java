606dbd73655378ed416e8d7452961d6038f8f081
AzureAD\CatalinTrif <ctrif@ecityclic.com>
2024-06-11 12:29:43 +0200

    test: agrega la clase de prueba del proyecto

---
+++ src/test/java/com/ectityclic/catademo/CatademoApplicationTests.java
@@ -0,0 +1,11 @@
+package com.ectityclic.catademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.ecityclic.catademo.controller.Controller;
+
+@SpringBootTest
+class CatademoApplicationTests {
+
+}
