614f4421480333191b0e11a2632282f2f9f86de4
AzureAD\CatalinTrif <ctrif@ecityclic.com>
2024-06-13 12:46:29 +0200

    dev: creació de la classe EmployeeInfoEntity i la relació 1 a 1 amb EmployeeEntity

--- src/main/java/com/ecityclic/catademo/entities/EmployeeInfoEntity.java
+++ src/main/java/com/ecityclic/catademo/entities/EmployeeInfoEntity.java
@@ -1,5 +1,45 @@
 package com.ecityclic.catademo.entities;
 
-public class EmployeeInfoEntity {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Builder
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name="employeeinfo")
+public class EmployeeInfoEntity implements Serializable {
+	
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="ID")
+	private Long id;
+	
+	private String name;
+	
+	@Column(name="fecha_nacimiento")
+	private String fecha;
+	
+	
+    @OneToOne
+    @JoinColumn(name = "id", referencedColumnName = "id")
+    private EmployeeEntity ee;
 
 }
